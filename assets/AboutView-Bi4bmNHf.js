233c19b3951336f97f8f7d1c254f106866d95bc4
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-04-28 12:25:11 +0100

    Updates

---
+++ assets/AboutView-Bi4bmNHf.js
@@ -0,0 +1,1 @@
+import{_ as e,f as s,e as t,g as o}from"./index-QV5GA_dy.js";const c={},a={class:"about"},n=o("h1",null,"This is an about page",-1),_=[n];function r(i,d){return t(),s("div",a,_)}const l=e(c,[["render",r]]);export{l as default};
